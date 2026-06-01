8c3564f9f780bbc2c6f6ef23e4a349790b4f6c39
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-06-01 07:47:19 +0545

    Dictionary added

--- Day-2.py
+++ Day-2.py
@@ -29,7 +29,54 @@ print("Only in course_A",course_A - course_B)
 print("Only in one course",course_A ^ course_B)
 
 # Remove Duplicates form list using set  
-scores_with_duplicates = {85,92,85,78,92,95,85}
+scores_with_duplicates = [85,92,85,78,92,95,85]
 unique_scores = list(set(scores_with_duplicates))
 print("\nOriginal scores:",scores_with_duplicates)
 print("Unique scores:",unique_scores)
+
+
+# Dictionaries 
+
+print("\n---Dictionaries---")
+print("="*50)
+
+#Dictionaries store key-value pairs and are very useful for structured data
+# Dictionnaries store data with keys
+
+student = {
+    "name" : "Alice",
+    "age" : "20",
+    "scores" : [85,92,78],
+    "department" : "computer science",
+    "is_active" : "yes",
+    
+}
+
+print("Student Dictonary:")
+print(student)
+
+
+# Accessing values
+print("\nStudents name:",student['name'])
+print("Student Scores:",student['scores'])
+print("Average Scores:", sum(student['scores'])/len(student['scores']))
+
+# Adding/updationg values 
+student["grade"] = "A"
+student["age"] = 21
+student["Hobbies"] = "Reading books"
+print("Hobbies are:",student['Hobbies'])
+print("\nAfter update:",student)
+
+greater_score = [score for score in student['scores'] if (score >=80)]
+print("greaater_score",greater_score)
+# College Dictionary
+
+college = {
+    "name" : "Shahid Smarak College",
+    "faculty" : ["BCA", "BBS", "BSW"],
+    "adress" : "Kirtipur",
+
+
+
+}
